48a54db09f95e0eaae00bda680fff0c136c7f3de
Matthew <57062385+TheMatthattanProject@users.noreply.github.com>
2022-11-28 14:55:04 +1100

    Reporting SQL
     - Startup scripts provide the base database
        - CREATE_TABLES
        - INSERT_TABLES
        - DROP_TABLES
    
     - Business Reporting SQL can be found and run from ./Reporting_SQL

---
+++ task_three/startupScripts/INSERT_TABLES.sql
@@ -0,0 +1,15 @@
+INSERT INTO Broker ('Name') VALUES ('Ted');
+INSERT INTO Broker ('Name') VALUES ('Mark');
+INSERT INTO Broker ('Name') VALUES ('Aaron');
+INSERT INTO Broker ('Name') VALUES ('Luke');
+
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('sam', 3000, 4);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('john', 4000, 2);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('mack', 5000, 2);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('test', 3000, 3);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('june', 2000, 3);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('mike', 4000, 1);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('annie', 4000, 2);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('michael', 2000, 1);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('tom', 2000, 4);
+INSERT INTO Customer ('Name', 'Amount', 'BrokerID') VALUES ('jason', 6000, 4);
