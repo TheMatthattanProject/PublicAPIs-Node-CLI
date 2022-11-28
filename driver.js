057950ca037c4f6a040ec282a86ed311d902298e
Matthew <57062385+TheMatthattanProject@users.noreply.github.com>
2022-11-28 14:16:18 +1100

    Sort and filter data
     - Use stub input for filtering
     - Filters by category, sorted alphabetically by API and then shows top {limit} results

--- driver.js
+++ driver.js
@@ -1,6 +1,26 @@
-const API_ENDPOINT = `https://api.publicapis.org/entries`
+const API_ENDPOINT = `https://api.publicapis.org/entries`;
+const limit = 5;
+const selected_category = "Anime";
 
-const res = await fetch(`${API_ENDPOINT}`)
-const json = await res.json()
 
-console.log(json)
+
+const res = await fetch(`${API_ENDPOINT}`);
+const json = await res.json();
+
+// Complete response may be needed later, however, this will mean less typing
+// and therefore reduces chances of typo causing issues down the line
+const entries = json['entries'];
+
+
+// Reduce dataset size before alphabetically sorting - reduces computation needed
+const category_filter = entries.filter((val) => { return val['Category'] === selected_category });
+
+// Use localeCompare to sort data alphabetically
+const alphabet_sort = category_filter.sort((a,b) => { a['API'].localeCompare(b['API'])});
+
+// Limit remaining data - needs to be last
+const limitedResults = alphabet_sort.filter((val, index) => { return index < limit });
+
+const retVal = limitedResults.length < 1 ? "No results": limitedResults;
+
+console.log(retVal);
